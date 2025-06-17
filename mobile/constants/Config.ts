497e1a6ae793a271feb00a97b7ab23c793f7c796
Rom943 <roma19943@gmail.com>
2025-06-17 21:10:14 +0300

    !

--- mobile/constants/Config.ts
+++ mobile/constants/Config.ts
@@ -3,7 +3,7 @@ export default {  // API URL - Replace with your actual backend URL when testing
   API_URL: {
     // For mobile devices, we need to use the network IP of the server
     development: 'http://10.100.102.13:3000',  // Use your computer's network IP
-    production: 'https://api.tadiran.com/api',
+    production: 'https://tadiran-invoice-warranty-system.onrender.com/api',
   },
     // API timeout in milliseconds
   API_TIMEOUT: 30000, // 30 seconds for file uploads
